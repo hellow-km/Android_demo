3caa7ca32f18045f96e88d57e0e5b84233154f5b
hzh-Neo <a1776376264@gmail.com>
2025-02-01 16:43:50 +0800

    init

---
+++ app/src/main/java/com/example/yunren/config.java
@@ -0,0 +1,8 @@
+package com.example.yunren;
+
+import com.example.yunren.BuildConfig;
+
+public class config {
+   //"https://www.yunren.online:14243/"
+   public static String serverUrl="https://www.yunren.online:14243/";// BuildConfig.is_pro?"https://www.yunren.online:14243/":"http://192.168.2.8:3000/";
+}
